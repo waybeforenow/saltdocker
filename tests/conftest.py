7fdfbcf992ae167c934207567a2216c1675184d9
Chris Larsen <clarsen@saltstack.com>
2020-06-12 10:32:00 -0600

    chore: blacken all python code
    * add `lint` and `black` makefile targets
    * pass all python code through `black`
    * run `make lint` on travis
    
    closes #26

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,11 +17,21 @@ def date():
 
 @pytest.fixture
 def versions():
-    data = json.load(urllib.request.urlopen('https://pypi.org/pypi/salt/json'))
+    data = json.load(urllib.request.urlopen("https://pypi.org/pypi/salt/json"))
     SaltVersion.data = data
+
     def make_name(version):
-        return f'saltstack/salt:{version}'
-    return map(make_name, sorted(filter(SaltVersion._check_version, map(distutils.version.LooseVersion, data['releases']))))
+        return f"saltstack/salt:{version}"
+
+    return map(
+        make_name,
+        sorted(
+            filter(
+                SaltVersion._check_version,
+                map(distutils.version.LooseVersion, data["releases"]),
+            )
+        ),
+    )
 
 
 @pytest.fixture
